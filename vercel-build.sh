78898febf60a3fdd3df7e119541f098baf6a8343
mitama <loligothick@gmail.com>
2026-07-11 09:01:11 +0900

    chore(des-glaneuses): temp

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH="/rust/bin:$PATH"
+rustup target add wasm32-unknown-unknown
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 pnpm build:wasm && pnpm build:app
