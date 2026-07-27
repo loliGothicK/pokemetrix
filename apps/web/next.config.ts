4d5bf47e05f4826a0fb66352062df3b43195b16a
mitama <loligothick@gmail.com>
2026-07-27 22:53:32 +0900

    feat: ???

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -8,8 +8,7 @@ import { withContentCollections } from "@content-collections/next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
   images: {
-    loader: "custom",
-    loaderFile: "./src/utils/ImageRoader.ts",
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https" as const,
